860685d8d84c73dc638cc1b907ef5f4a2882d104
Artur Grzes <grzesa@poczta.onet.pl>
2017-06-10 11:08:31 +0200

    commit

---
+++ js/typydanych.js
@@ -0,0 +1,37 @@
+'use strict'
+
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+
+
+var wyplataStr = "5000";
+var premiaStr = "2500";
+var calkowitaWyplataStr;
+
+calkowitaWyplataStr = wyplataStr + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+
+var imieStudenta = " Michał ";
+console.log(imieStudenta.indexOf('c'));
+console.log(imieStudenta);
+console.log(imieStudenta.trim());
+
+var czyJestSmog;
+czyJestSmog = true;
+
+if (czyJestSmog) {
+    console.log("Jest SMOG");
+} else {
+    console.log("Nie ma SMOGa ;)");
+}
+
+
+
